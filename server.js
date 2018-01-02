9057f520ebac54b80b281a7ad81a8f84918d9270
Rik Brutsaert <hb31@cornell.edu>
2018-01-01 21:11:02 -0700

    changed port for heroku and package to support heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const hbs=require("hbs");
 const fs =require('fs');
-
+const port=process.env.PORT || 3000;
 var app=express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -17,10 +17,10 @@ app.use((req,res,next)=>{
 });
 
 
-app.use((req,res,next)=>{
-
-    res.render('maintenance.hbs');
-});
+// app.use((req,res,next)=>{
+//
+//     res.render('maintenance.hbs');
+// });
 app.use(express.static(__dirname+'/public'));
 
 hbs.registerHelper('getCurrentYear', ()=>{
@@ -54,4 +54,4 @@ app.get('/bad',(req,res)=>{
   });
 });
 
-app.listen(3000,()=>{console.log('server is up on port 3000')});
+app.listen(port,()=>{console.log(`server is up on port ${port}`)});
